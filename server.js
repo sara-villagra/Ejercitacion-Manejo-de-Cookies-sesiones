77c94ba2729ebe04367aa8d639d4c956a0966948
sarahvill <130417177+sara-villagra@users.noreply.github.com>
2024-06-26 23:13:12 -0300

    agregue archivo login.ejs, ejs como dependencia,middleware

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express()
 
 app.use(cookieParser())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // Permite parsear los datos desde un formulario
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
 
 app.get('/', (req, res) => {
  res
